b947f170cab8274513db819c7f190b9a37f9d91f
XiaoPengLi <adamin1990@gmail.com>
2016-06-03 23:06:44 +0800

    fix main

--- app/src/main/java/com/adamin/android/qdbus/view/MainActivity.java
+++ app/src/main/java/com/adamin/android/qdbus/view/MainActivity.java
@@ -135,20 +135,20 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
+//
+//        if (id == R.id.nav_camera) {
+//            // Handle the camera action
+//        } else if (id == R.id.nav_gallery) {
+//
+//        } else if (id == R.id.nav_slideshow) {
+//
+//        } else if (id == R.id.nav_manage) {
+//
+//        } else if (id == R.id.nav_share) {
+//
+//        } else if (id == R.id.nav_send) {
+//
+//        }
 
        drawerLayout.closeDrawer(GravityCompat.START);
         return true;
